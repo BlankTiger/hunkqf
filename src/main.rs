a4b9b62b5dbd8d2fc6400a3f9e69c8a553754606
Maciej Urban <maciej.urban@nokia.com>
2024-01-04 18:19:23 +0100

    feat: getting quickfix of git diff hunks for neovim

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::process::{Command, Stdio};
+
 fn main() {
-    println!("Hello, world!");
+    let cmd_output = Command::new("git")
+        .arg("diff")
+        .stdout(Stdio::piped())
+        .output()
+        .expect("there to be outpuut")
+        .stdout;
+    let diff = std::str::from_utf8(&cmd_output).unwrap();
+    let locs = parse_locs(diff);
+    for loc in locs {
+        println!("{loc}");
+    }
+}
+
+fn parse_locs(diff: &str) -> Vec<String> {
+    diff.split("+++ b/")
+        .enumerate()
+        .filter_map(|(idx, l)| if idx % 2 != 0 { Some(l) } else { None })
+        .map(|loc| {
+            let (filename, rest) = loc.split_once('\n').unwrap();
+            let (loc_line, code) = rest.split_once('\n').unwrap();
+            let (_, location_dirty) = loc_line.split_once('+').unwrap();
+            let location = location_dirty.split(',').next().unwrap();
+            let code_line = code.split('\n').next().unwrap();
+            let code_line = if code_line.starts_with('+') {
+                code_line.strip_prefix('+').unwrap()
+            } else if code_line.starts_with('-') {
+                code_line.strip_prefix('-').unwrap()
+            } else {
+                code_line
+            };
+            format!("{filename}:{location}:0: {code_line}")
+        })
+        .collect()
 }
